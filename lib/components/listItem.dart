23092cad49f94e48567b3423c79687b91bb09ce5
igutzzz <igor.almeida@madeinweb.app>
2023-07-26 13:23:00 -0300

    initial commit

---
+++ lib/components/listItem.dart
@@ -0,0 +1,32 @@
+import 'package:estudos_flutter/main.dart';
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  final String title;
+
+  const ListItem({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title),
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                ListNotification(title).dispatch(context);
+              },
+              splashRadius: 20,
+            )
+          ],
+        ),
+        tileColor: Colors.grey,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:estudos_flutter/components/listItem.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Tasks());
+}
+
+class ListNotification extends Notification {
+  final String task;
+  ListNotification(this.task);
+}
+
+class Tasks extends StatefulWidget {
+  const Tasks({super.key});
+
+  @override
+  State<Tasks> createState() => _TasksState();
+}
+
+class _TasksState extends State<Tasks> {
+  List<String> tasks = [];
+  final textController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Task Manager"),
+        ),
+        body: NotificationListener<ListNotification>(
+          onNotification: (n) {
+            setState(() {
+              tasks.remove(n.task);
+              print(tasks);
+            });
+            return true;
+          },
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          controller: textController,
+                          onSubmitted: (text) {
+                            setState(() {
+                                if (textController.text != "") {
+                                  tasks.add(textController.text);
+                                }
+                                textController.clear();
+                                print(tasks);
+                              });
+                          },
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Nova tarefa..."),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (textController.text != "") {
+                                  tasks.add(textController.text);
+                                }
+                                textController.clear();
+                                print(tasks);
+                              });
+                            },
+                            child: const Icon(Icons.add)),
+                      )
+                    ],
+                  ),
+                ),
+                ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 200, minHeight: 56.0),
+                  child: ListView.builder(
+                      itemCount: tasks.length,
+                      itemBuilder: (context, index) {
+                        var title = tasks[index];
+                        return ListItem(title: title);
+                      }),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
